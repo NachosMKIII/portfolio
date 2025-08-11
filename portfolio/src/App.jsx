b6df1f74bfb3fb6835a7439b69898e6ef66151c2
NachosMKIII <nachosmkii@gmail.com>
2025-08-11 20:47:39 -0300

    Restart

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,22 +1,13 @@
 //App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FantasyHeader from "./components/fantasy-header";
-
-import AboutMe from "./components/AboutMe";
-import Projects from "./components/Projects";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="bg-[url('./assets/images/cool-wizard-night.png')] w-screen min-h-screen bg-center bg-cover bg-no-repeat overflow-x-hidden">
-        <FantasyHeader />
-        <Routes>
-          <Route path="/AboutMe" element={<AboutMe />} />
-          <Route path="/Projects" element={<Projects />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div>
+      <img src="./upper-teeth-1.png" />"
+      <img src="./assets/upper-teeth-1.png" />"
+    </div>
   );
 };
 
