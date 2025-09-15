91a6720c81e64a7af63b398b60ca4dcaf7ae59b0
NachosMKIII <nachosmkii@gmail.com>
2025-09-15 09:01:57 -0300

    removed the "hide" buttons

--- portfolio/src/components/AboutMe.jsx
+++ portfolio/src/components/AboutMe.jsx
@@ -10,8 +10,8 @@ const AboutMe = () => {
   return (
     <div className="w-[50vw] flex">
       <div
-        className={`bg-neutral-900 ml-40 mt-20 grid grid-cols-1 rounded ${
-          isExpanded ? "w-50 h-50" : "w-30 h-10"
+        className={`bg-neutral-900 ml-40 mt-20 rounded ${
+          isExpanded ? "w-100 h-100" : "w-30 h-10"
         } overflow-hidden transition-all transition-discrete duration-700 bold text-slate-200`}
       >
         <h1
@@ -21,14 +21,10 @@ const AboutMe = () => {
           About Me
         </h1>
         <p className="p-2 pt-3 mb-10">
-          I'm a junior software developer from Brazil,
+          I'm a junior software developer from Brazil, i'm currently in my last
+          year of school(I'm 18) and i'm looking forward to getting in the tech
+          industry, i have been studying programming for over a year
         </p>
-        <button
-          className="bg-neutral-800 cursor-pointer place-self-end p-2"
-          onClick={toggleExpand}
-        >
-          Hide
-        </button>
       </div>
     </div>
   );
