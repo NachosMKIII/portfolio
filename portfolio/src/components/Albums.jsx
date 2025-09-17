505b0e44d637576de2076d7e04cd868c7cb95ce6
NachosMKIII <nachosmkii@gmail.com>
2025-09-17 09:33:26 -0300

    added an "albums i like" section

--- portfolio/src/components/Albums.jsx
+++ portfolio/src/components/Albums.jsx
@@ -6,7 +6,7 @@ const Albums = () => {
 
   const scroll = (direction) => {
     if (scrollRef.current) {
-      const scrollAmount = 176;
+      const scrollAmount = 208;
       scrollRef.current.scrollBy({
         left: direction === "left" ? -scrollAmount : scrollAmount,
         behavior: "smooth",
@@ -16,8 +16,8 @@ const Albums = () => {
 
   return (
     <div className=" w-[50vw]">
-      <h1 className="ml-12 mt-5 w-54 text-xl rounded-full bg-neutral-950 p-2 px-6 text-neutral-200">
-        Games i really like
+      <h1 className="ml-12 mt-5 w-109 line-clamp-2 text-xl rounded-full bg-neutral-950 p-2 px-6 text-neutral-200">
+        Albums i really like (from bands i really like)
       </h1>
       <div className="flex gap-4 ml-2 w-[49vw] mt-4">
         <button
@@ -31,56 +31,55 @@ const Albums = () => {
           ref={scrollRef}
         >
           <a
-            href="https://en.wikipedia.org/wiki/Kenshi_(video_game)"
+            href="https://open.spotify.com/track/0NCcbiW3kiasTzgg3CiBrf"
             target="_blank"
             rel="noopener noreferrer"
             className="flex-shrink-0 mb-2"
           >
             <img
-              src="/kenshi.jpeg"
-              href="https://en.wikipedia.org/wiki/Kenshi_(video_game)"
-              className="w-40 mt-4 mb-4 h-52 cursor-pointer"
+              src="/only-place.jpg"
+              className="w-52 mt-4 mb-4 h-52 cursor-pointer"
             />
           </a>
           <a
-            href="https://en.wikipedia.org/wiki/The_Elder_Scrolls_V:_Skyrim"
+            href="https://open.spotify.com/album/6EtrZFZ6FMR6fbB82oHUWi"
             target="_blank"
             rel="noopener noreferrer"
             className="flex-shrink-0 mb-2"
           >
-            <img src="/skyrim.png" className="w-40 h-52 mb-4 mt-4" />
+            <img src="/cassette-city.jpg" className="w-52 h-52 mb-4 mt-4" />
           </a>
           <a
-            href="https://en.wikipedia.org/wiki/Stardew_Valley"
+            href="https://open.spotify.com/album/5tTmosmyV33wCiwniZc4Qs"
             target="_blank"
             rel="noopener noreferrer"
             className="flex-shrink-0 mb-2"
           >
             <img
-              src="/stardew-valley.jpeg"
-              className="w-40 mt-4 h-52 cursor-pointer"
+              src="/depression-cherry.jpg"
+              className="w-52 mt-4 h-52 cursor-pointer"
             />
           </a>
           <a
-            href="https://en.wikipedia.org/wiki/Fear_%26_Hunger#Fear_&_Hunger_2:_Termina"
+            href="https://open.spotify.com/album/1CbtBQqibzdxhmpiLD7vzN"
             target="_blank"
             rel="noopener noreferrer"
             className="flex-shrink-0 mb-2"
           >
             <img
-              src="/termina.jpeg"
-              className="w-40 mt-4 h-52 cursor-pointer"
+              src="/hawaii-part-ii.jpg"
+              className="w-52 mt-4 h-52 cursor-pointer"
             />
           </a>
           <a
-            href="https://en.wikipedia.org/wiki/Ultrakill"
+            href="https://open.spotify.com/album/4XriwpnB6fJm8OMxb7wQcV"
             target="_blank"
             rel="noopener noreferrer"
             className="flex-shrink-0 mb-2"
           >
             <img
-              src="/ultrakill.webp"
-              className="w-40 mt-4 h-52 cursor-pointer"
+              src="/impossible-animals.jpg"
+              className="w-52 mt-4 h-52 cursor-pointer"
             />
           </a>
           <a
@@ -89,7 +88,45 @@ const Albums = () => {
             rel="noopener noreferrer"
             className="flex-shrink-0 mb-2"
           >
-            <img src="/dmc3.jpg" className="w-40 mt-4 h-52 cursor-pointer" />
+            <img src="/lighght.jpg" className="w-52 mt-4 h-52 cursor-pointer" />
+          </a>
+          <a
+            href="https://en.wikipedia.org/wiki/Devil_May_Cry_3:_Dante%27s_Awakening"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex-shrink-0 mb-2"
+          >
+            <img
+              src="/next-thing.jpg"
+              className="w-52 mt-4 h-52 cursor-pointer"
+            />
+          </a>
+          <a
+            href="https://en.wikipedia.org/wiki/Devil_May_Cry_3:_Dante%27s_Awakening"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex-shrink-0 mb-2"
+          >
+            <img src="pop-food.jpg" className="w-52 mt-4 h-52 cursor-pointer" />
+          </a>
+          <a
+            href="https://en.wikipedia.org/wiki/Devil_May_Cry_3:_Dante%27s_Awakening"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex-shrink-0 mb-2"
+          >
+            <img src="/OWUWAYN.jpg" className="w-52 mt-4 h-52 cursor-pointer" />
+          </a>
+          <a
+            href="https://en.wikipedia.org/wiki/Devil_May_Cry_3:_Dante%27s_Awakening"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex-shrink-0 mb-2"
+          >
+            <img
+              src="/i-hate-jazz.jpg"
+              className="w-52 mt-4 h-52 cursor-pointer"
+            />
           </a>
         </div>
         <button
